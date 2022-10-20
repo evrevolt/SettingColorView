82127d79ec90825a3c49d8d3479194493aa76f23
Evrevolt <gennadi_wow@mail.ru>
2022-10-20 20:05:22 +0300

    Initial Commit

---
+++ SettingColorView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingColorView
+//
+//  Created by Геннадий Ведерников on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
